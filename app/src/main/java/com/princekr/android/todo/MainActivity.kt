a9e1769e06f8cd96d17e4cdf16488baea755353f
prince <prince18pat@gmail.com>
2019-08-11 19:10:51 +0900

    initial project created

---
+++ app/src/main/java/com/princekr/android/todo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.princekr.android.todo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
