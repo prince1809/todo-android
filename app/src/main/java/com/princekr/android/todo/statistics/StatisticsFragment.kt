cabc2092fa1e3ba3cc55e590265360a052bd411a
prince <prince18pat@gmail.com>
2019-08-12 22:02:35 +0900

    add layout for navigation and update task activity

---
+++ app/src/main/java/com/princekr/android/todo/statistics/StatisticsFragment.kt
@@ -0,0 +1,13 @@
+package com.princekr.android.todo.statistics
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: StatisticsFragment
+
+    //private val viewModel by viewModels<Statist> {  }
+}
